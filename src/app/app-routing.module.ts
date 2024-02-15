607725e29f5b73aabd2963050b1d4c8682e69762
kapibarra2315 <nikita.nov2016@yandex.ru>
2024-02-15 15:02:47 +0300

    fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // перенаправление на главную страницу по умолчанию
+  { path: 'home', component: MainPageComponent }, // маршрут для главной страницы
+  // { path: 'services', component:  }, // маршрут для страницы услуг
+  // { path: 'about', component:  }, // маршрут для страницы "О нас"
+  // { path: 'contacts', component:  }, // маршрут для страницы контактов
+  // { path: '**', component:  } // маршрут для несуществующих страниц
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

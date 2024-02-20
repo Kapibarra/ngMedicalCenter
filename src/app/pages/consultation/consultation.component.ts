5ce1eaf953de8feba2130cadd7bf87619807e6cb
kapibarra2315 <nikita.nov2016@yandex.ru>
2024-02-20 13:21:33 +0300

    new page

--- src/app/pages/consultation/consultation.component.ts
+++ src/app/pages/consultation/consultation.component.ts
@@ -7,25 +7,85 @@ import { Component } from '@angular/core';
 })
 export class ConsultationComponent {
   price = [
-    { serviceName: 'Консультация терапевта', price: 1500 },
-    { serviceName: 'Консультация кардиолога', price: 2000 },
-    { serviceName: 'Консультация гинеколога', price: 1800 },
-    { serviceName: 'Консультация офтальмолога', price: 1700 },
-    { serviceName: 'Консультация уролога', price: 1600 },
-    { serviceName: 'Консультация невролога', price: 1700 },
-    { serviceName: 'Консультация педиатра', price: 1400 },
-    { serviceName: 'Консультация лора', price: 1600 },
-    { serviceName: 'Консультация стоматолога', price: 1800 },
-    { serviceName: 'Консультация аллерголога', price: 1700 },
-    { serviceName: 'Консультация дерматолога', price: 1600 },
-    { serviceName: 'Консультация эндокринолога', price: 1800 },
-    { serviceName: 'Консультация гастроэнтеролога', price: 1700 },
-    { serviceName: 'Консультация травматолога', price: 1900 },
-    { serviceName: 'Консультация хирурга', price: 2000 },
-    { serviceName: 'Консультация онколога', price: 2200 },
-    { serviceName: 'Консультация психиатра', price: 1800 },
-    { serviceName: 'Консультация психолога', price: 1600 },
-    { serviceName: 'Консультация гинеколога-эндокринолога', price: 1900 },
-    { serviceName: 'Консультация невропатолога', price: 1700 },
+    {
+      serviceName: 'Консультация терапевта',
+      price: 1500,
+      duration: '30 минут',
+    },
+    {
+      serviceName: 'Консультация кардиолога',
+      price: 2000,
+      duration: '40 минут',
+    },
+    {
+      serviceName: 'Консультация гинеколога',
+      price: 1800,
+      duration: '30 минут',
+    },
+    {
+      serviceName: 'Консультация офтальмолога',
+      price: 1700,
+      duration: '30 минут',
+    },
+    { serviceName: 'Консультация уролога', price: 1600, duration: '30 минут' },
+    {
+      serviceName: 'Консультация невролога',
+      price: 1700,
+      duration: '40 минут',
+    },
+    { serviceName: 'Консультация педиатра', price: 1400, duration: '30 минут' },
+    { serviceName: 'Консультация лора', price: 1600, duration: '30 минут' },
+    {
+      serviceName: 'Консультация стоматолога',
+      price: 1800,
+      duration: '40 минут',
+    },
+    {
+      serviceName: 'Консультация аллерголога',
+      price: 1700,
+      duration: '30 минут',
+    },
+    {
+      serviceName: 'Консультация дерматолога',
+      price: 1600,
+      duration: '30 минут',
+    },
+    {
+      serviceName: 'Консультация эндокринолога',
+      price: 1800,
+      duration: '40 минут',
+    },
+    {
+      serviceName: 'Консультация гастроэнтеролога',
+      price: 1700,
+      duration: '30 минут',
+    },
+    {
+      serviceName: 'Консультация травматолога',
+      price: 1900,
+      duration: '40 минут',
+    },
+    { serviceName: 'Консультация хирурга', price: 2000, duration: '40 минут' },
+    { serviceName: 'Консультация онколога', price: 2200, duration: '60 минут' },
+    {
+      serviceName: 'Консультация психиатра',
+      price: 1800,
+      duration: '30 минут',
+    },
+    {
+      serviceName: 'Консультация психолога',
+      price: 1600,
+      duration: '30 минут',
+    },
+    {
+      serviceName: 'Консультация гинеколога-эндокринолога',
+      price: 1900,
+      duration: '40 минут',
+    },
+    {
+      serviceName: 'Консультация невропатолога',
+      price: 1700,
+      duration: '30 минут',
+    },
   ];
 }
